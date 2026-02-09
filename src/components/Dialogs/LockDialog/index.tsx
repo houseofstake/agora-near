35d5f8a1beec66fa280c0abebe0275aed0958b0d
Atomauro <mauro.henaog@gmail.com>
2026-02-09 08:50:11 -0500

    feat(analytics): update lock dialog entry point to use analytics hook

--- src/components/Dialogs/LockDialog/index.tsx
+++ src/components/Dialogs/LockDialog/index.tsx
@@ -3,8 +3,9 @@ import { LockDialogContent } from "./LockDialogContent";
 import { MaintenanceDialog } from "./MaintenanceDialog";
 import Tenant from "@/lib/tenant/tenant";
 import { useEffect } from "react";
-import { MixpanelEvents } from "@/lib/analytics/mixpanel";
-import { trackEvent } from "@/lib/analytics";
+import Big from "big.js";
+import { useAnalytics } from "@/hooks/useAnalytics";
+import { useNear } from "@/contexts/NearContext";
 
 export type LockDialogSource =
   | "onboarding"
@@ -20,17 +21,21 @@ type NearLockDialogProps = {
 
 export const NearLockDialog = (props: NearLockDialogProps) => {
   const tenant = Tenant.current();
+  const { trackLockingFlowStarted } = useAnalytics();
+  const { signedAccountId, getBalance } = useNear();
 
   useEffect(() => {
-    trackEvent({
-      event_name: MixpanelEvents.StartedLockAndStake,
-      event_data: {
-        source: props.source,
-        preSelectedTokenId: props.preSelectedTokenId,
-        customStakingPoolId: props.customStakingPoolId,
-      },
-    });
-  }, [props.source, props.preSelectedTokenId, props.customStakingPoolId]);
+    // Check balance if connected
+    let hasBalance = false;
+    const checkBalance = async () => {
+        if (signedAccountId) {
+            const balance = await getBalance(signedAccountId);
+            hasBalance = Big(balance).gt(0);
+        }
+        trackLockingFlowStarted(props.source, !!signedAccountId, hasBalance);
+    }
+    checkBalance();
+  }, [props.source, signedAccountId, getBalance, trackLockingFlowStarted]);
 
   if (tenant.isMaintenanceMode) {
     return <MaintenanceDialog closeDialog={props.closeDialog} />;
